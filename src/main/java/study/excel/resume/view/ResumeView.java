beabfaf0c67a1fa6e1500bc2733b1af9940a6f1d
cgk95 <kjkj5868@gmail.com>
2023-10-09 15:14:18 +0900

    chore :: Scanner 클래스를 final로 지정

--- src/main/java/study/excel/resume/view/ResumeView.java
+++ src/main/java/study/excel/resume/view/ResumeView.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class ResumeView {
-    private Scanner sc;
+    private final Scanner sc;
 
     public ResumeView() {
         sc = new Scanner(System.in);
